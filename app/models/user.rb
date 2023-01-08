0ed861f534abb6f13aa4b0cacfa4a38ac1aa4ce1
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-01-08 21:42:33 +0700

    create Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name, :email, :password, presence: true
   validates :name, :email, uniqueness: true
   validates :password, length: { in: 6..20 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_08_141746) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_142812) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_08_141746) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
