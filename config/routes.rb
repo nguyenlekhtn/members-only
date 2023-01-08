025bedf301573ec921ec39fc1bf84efb0c767e70
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-01-08 22:42:06 +0700

    migrate devise changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   resources :post, only: [:new, :create, :index]
-  routes "posts#index"
+  root "posts#index"
 end
