c055fe6561ac8c49726d86a6d90d7d0c3ab7a6d3
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-01-12 06:56:42 +0700

    populate post params with user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    user_id = current_user.id
+    @post = Post.new({ **post_params, user_id: })
+    if @post.save
+      redirect_to @post
+    else
+      flash.now[:error] = "Could not save client"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  protected
+
+  def post_params
+    params.permit(:post).permit(:title, :body)
+  end
 end
