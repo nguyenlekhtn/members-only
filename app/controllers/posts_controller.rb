0e4d9cc46a5f9d9a5b5b635d845d9d0b9a210679
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-01-12 22:54:10 +0700

    add index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
@@ -9,7 +13,7 @@ class PostsController < ApplicationController
     user_id = current_user.id
     @post = Post.new({ **post_params, user_id: })
     if @post.save
-      redirect_to @post
+      redirect_to posts_path
     else
       flash.now[:error] = "Could not save client"
       render :new, status: :unprocessable_entity
@@ -19,6 +23,6 @@ class PostsController < ApplicationController
   protected
 
   def post_params
-    params.permit(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body)
   end
 end
