78440a4d4903aae31796faf0fb20a21b35d32c60
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-01-23 18:09:35 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create([
+  {name: "nle", email: "nle@mail.com", password: "123456"},
+  {name: "odin", email: "odin@mail.com", password: "theodinproject"}
+])
+
+(1..2).each do |i|
+  Post.create(title: "Post ##{i}", body: "This is the post number #{i}", user_id: i)
+end
